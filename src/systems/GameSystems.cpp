dea7cf31ac770690ade76a606588190ee8a453a8
Diego Santos Seabra <dss-diego@hotmail.com>
2023-02-21 06:20:19 -0300

    added game systems interface

---
+++ src/systems/GameSystems.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Diego S. Seabra on 21/02/23
+//
+
+#include "GameSystems.h"
+
+#include <iostream>
+
+void GameSystems::Update()
+{
+
+}
---
+++ src/systems/GameSystems.h
@@ -0,0 +1,14 @@
+//
+// Created by Diego S. Seabra on 21/02/23
+//
+
+#ifndef GAME_SYSTEMS_H
+#define GAME_SYSTEMS_H
+
+class GameSystems
+{
+public:
+	static void Update();
+};
+
+#endif GAME_SYSTEMS_H
